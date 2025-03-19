95162f99588595db70526a3ae2b3c043313f476f
Evans Ngugi <ngugie649@gmail.com>
2025-03-19 08:42:19 +0300

    addittional changes

--- src/components/file.py
+++ src/components/file.py
@@ -1,22 +1,25 @@
 import requests
-import json
 
-def login_and_get_cookies(email, password):
+def login_and_get_session(email, password):
+    session = requests.session()
     login_url = 'https://app.outlier.ai/en/expert/login'
     data = {
         'email': email,
         'password': password
     }
-    response = requests.post(login_url, data=data, allow_redirects=True)
+    response = session.post(login_url, data=data, allow_redirects=True)
+    print(f"Login response status code: {response.status_code}")
+    print(f"Login response URL: {response.url}")
+    print(f"Response text: {response.text}")
     if response.status_code == 200 and 'dashboard' in response.url:
         print("Login successful")
-        return response.cookies
+        return session
     else:
         print("Login failed")
         return None
 
-def fetch_json_data(url, cookies):
-    response = requests.get(url, cookies=cookies)
+def fetch_json_data(url, session):
+    response = session.get(url)
     if response.status_code == 200:
         json_data = response.json()
         return json_data
@@ -24,16 +27,13 @@ def fetch_json_data(url, cookies):
         print(f"Failed to fetch data. Status code: {response.status_code}")
         return None
 
-# User credentials
 email = 'reymondbrlz@gmail.com'
 password = 'Reymond123?'
+session = login_and_get_session(email, password)
 
-# Login and get cookies
-cookies = login_and_get_cookies(email, password)
-
-if cookies:
+if session:
     url = 'https://app.outlier.ai/internal/experts/worker-skills'
-    json_data = fetch_json_data(url, cookies)
+    json_data = fetch_json_data(url, session)
     if json_data:
         print("Fetched JSON data:", json_data)
     else:
