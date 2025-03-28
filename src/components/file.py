493336c544f52385b3695e72a0358e3d17f47e8a
Evans Ngugi <ngugie649@gmail.com>
2025-03-28 23:47:40 +0300

    addittional changes

--- src/components/file.py
+++ src/components/file.py
@@ -46,7 +46,3 @@ else:
     
     print("Web scraping is the process of extracting data from websites. It involves making HTTP requests to a web server, retrieving the HTML content of a webpage, and then parsing that content to extract the desired information. Web scraping can be done using various programming languages and libraries, such as Python with BeautifulSoup or Scrapy.")
     ## Cyber Security (Web Scraping) ## web penetration testing
-    ## Cyber Security (Web Scraping) ## web penetration testing
-    ## Cyber Security (Web Scraping) ## web penetration testing
-    ## Cyber Security (Web Scraping) ## web penetration testing
-    ## uyeiwtyej yftty hhtt uwtwue u6ewhw yetuwre kyetwtw uetet
