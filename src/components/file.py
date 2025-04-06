2df6793936196d0e17edc493cdf2a026bf3b7bb0
Evans Ngugi <ngugie649@gmail.com>
2025-04-06 09:47:59 +0300

    Blog page

--- src/components/file.py
+++ src/components/file.py
@@ -45,5 +45,4 @@ else:
     ## Cyber Security (Web Scraping) ## web penetration testing
     
     print("Web scraping is the process of extracting data from websites. It involves making HTTP requests to a web server, retrieving the HTML content of a webpage, and then parsing that content to extract the desired information. Web scraping can be done using various programming languages and libraries, such as Python with BeautifulSoup or Scrapy.")
-    ## Cyber Security (Web Scraping) ## web penetration testing
-    # continue
+    
