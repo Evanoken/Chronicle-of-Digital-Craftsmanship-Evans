5b3b0251adc4538cfc832c4a2aabf9e8b68b365d
Evans Ngugi <ngugie649@gmail.com>
2025-04-07 05:14:12 +0300

    deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Analytics } from "@vercel/analytics/react";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
