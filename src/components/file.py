aabd44dc415dc7dd0fd67fe762dd84824a200261
Evans Ngugi <ngugie649@gmail.com>
2025-06-13 22:19:47 -0700

    add new features

--- src/components/file.py
+++ src/components/file.py
@@ -52,5 +52,5 @@ def main():
         
 # new conct
         
-        . be very careful on the evalution you are giving it should based on what is in the responses not just guess work or assumptions
+        evalution you are giving it should based on what is in the responses not just guess work or assumptions
 
