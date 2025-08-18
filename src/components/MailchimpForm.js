e5b0e6d172782f1e6312720b87d2397ed5d916e0
Evans Ngugi <ngugie649@gmail.com>
2025-08-18 15:46:29 +0300

    message

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -17,6 +17,7 @@ export const MailchimpForm = () => {
             onSuccess={() => console.log("Mailchimp success")}
             onValidated={formData => subscribe(formData)}
             onError={error => console.error("Mailchimp error:", error)}
+            onSuccess={() => console.log("Mailchimp success")}
             />
         )}
         />
